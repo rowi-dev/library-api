5e7a8f3eb1990571216cf7d9884c20155c4aee1f
roshanwijendra <roshan2signup@gmail.com>
2024-05-23 20:17:37 +0800

    Generated Stratergy Added to the entities

--- src/main/java/com/rowi/library_api/model/Book.java
+++ src/main/java/com/rowi/library_api/model/Book.java
@@ -1,6 +1,8 @@
 package com.rowi.library_api.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -8,6 +10,7 @@ import lombok.Data;
 @Data
 public class Book {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String isbn;
     private String title;
